8277c3a84f55fe3b5c7d182e30efa4275948b59a
joshua <joshuatsn@gmail.com>
2013-04-22 17:08:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+Run these commands to try it out.
+
+```
+bundle
+rake db:setup
+rails s
+```
+# Do seed after the migrate
+Requires Ruby 1.9.2 or later to run.
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Joseph = Article.create! name: "Joseph Wachira", content: <<-ARTICLE
+Ever wondered how the #Rails stack boots up for a #RubyOnRails application? Here's some interesting reading
+ARTICLE
+
+john = Article.create! name: "john", content: <<-ARTICLE
+"I've earned 'Models Taste Like Chicken' badge on Rails for Zombies
+ARTICLE
+
+victor = Article.create! name: "victor", content: <<-ARTICLE
+Software Engineers, Scalable Data\/#Analytics, Java, Scala, PostgreSQL, Hibernate, Spring, OSGi, Maven, Groovy, Grails, #RubyOnRails
+ARTICLE
+
+lex_luthor = Article.create! name: "Lex Luthor", content: <<-ARTICLE
+@Killukru @EdenServers La magnifique doc de RubyOnRails? Ce n'est m\u00eame pas une erreur SQL mais une erreur qui sp\u00e9cifie le serveur introuvabl
+ARTICLE
